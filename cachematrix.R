8c502e74ee3e65e38dce8bf6fbb55b27f09d6440
DRMalvern <103321608+DRMalvern@users.noreply.github.com>
2022-04-25 20:40:11 +0200

    my Assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,28 @@
-## Combination of functions makeCacheMatrix and cacheSolve allows calculate
-## inverse matrix only one times for each matrix. Second and next requests 
-## for inverse matrix are answered from cache.This solution save time.
 
-
-## Function makeCacheMatrix creates object that can cache inverse matrix
-## after calculating for next use.
 makeCacheMatrix <- function(x = matrix()) {
-    ## Variable inverseMatrix have to be initialized by value NULL 
-    ## (for first ask for result of inverting matrix is necessary). 
+    makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+   
     inverseMatrix <- NULL
     
-    ## Function setMatrix for reinitialize matrix and 
-    ## set flag about not calculated inverse matrix (inverseMatrix <- NULL). 
+  
     setMatrix <- function(y) {
         x <<- y
         inverseMatrix <<- NULL
     }
     
-    ## Name of next functions describe what they do.
+    
     getMatrix <- function() x
     setInverseMatrix <- function(im) inverseMatrix <<- im
     getInverseMatrix <- function() inverseMatrix
@@ -27,25 +32,32 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Function cacheSolve first time calculate inverse matrix 
-## and next time will use calculated value again. 
-## Remember value is much faster than calculate again.
+
 cacheSolve <- function(x, ...) {
-    ##Receiving inverse matrix or NULL value. 
+     
     im <- x$getInverseMatrix()
     
-    ## Test whether it has been inverse matrix calculated
-    ## (NULL==not calculated). 
+    
     if(!is.null(im)) {
         message("getting cached data")
         return(im)
     }
     
-    ##First calculation of inverse matrix and save result for next use.
+    cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
     data <- x$getMatrix()
     im <- solve(data, ...)
     x$setInverseMatrix(im)
     
-    ## Return a matrix that is the inverse of 'x'
+    
     im
 }
